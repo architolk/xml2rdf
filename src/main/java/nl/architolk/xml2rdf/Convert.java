ed6ac7937d4110137baf5e55b8353b11d5009cc2
architolk <msj@xs4all.nl>
2022-07-22 10:46:29 +0200

    Adding support for html processing

--- src/main/java/nl/architolk/xml2rdf/Convert.java
+++ src/main/java/nl/architolk/xml2rdf/Convert.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.StringReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import javax.xml.transform.OutputKeys;
@@ -11,6 +12,12 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.DocumentType;
+import org.jsoup.safety.Cleaner;
+import org.jsoup.safety.Safelist;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.RDFDataMgr;
@@ -35,11 +42,22 @@ public class Convert {
       }
 
       try {
+        StreamSource inputSource;
+        if (FilenameUtils.getExtension(args[0]).equals("html")) {
+          LOG.info("Input file is HTML");
+          Cleaner cleaner = new Cleaner(Safelist.relaxed().addAttributes(":all","class"));
+          Document doc = cleaner.clean(Jsoup.parse(new File(args[0]),"UTF-8","http://example.com/"));
+          doc.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
+          inputSource = new StreamSource(new StringReader(doc.html().replace("&nbsp;"," ")));
+          System.out.println(doc.html());
+        } else {
+          inputSource = new StreamSource(new File(args[0]));
+        }
         ByteArrayOutputStream outBuffer = new ByteArrayOutputStream();
         if (args.length == 2) {
-          XmlEngine.transform(new StreamSource(new File(args[0])),"xsl/xml2rdf.xsl",new StreamResult(outBuffer));
+          XmlEngine.transform(inputSource,"xsl/xml2rdf.xsl",new StreamResult(outBuffer));
         } else {
-          XmlEngine.transform(new StreamSource(new File(args[0])),new StreamSource(new File(args[2])),new StreamResult(outBuffer));
+          XmlEngine.transform(inputSource,new StreamSource(new File(args[2])),new StreamResult(outBuffer));
         }
         ByteArrayInputStream inBuffer = new ByteArrayInputStream(outBuffer.toByteArray());
         Model model = ModelFactory.createDefaultModel();
